e71827afed26bbd9a506483b75a739d5738a9941
mattjacobs93 <matthewjacobs73@yahoo.com>
2022-02-06 21:36:07 -0800

    add AI

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const EASY_DEPTH = 1
 const MEDIUM_DEPTH = 4
 const HARD_DEPTH = 9
 const chars = ['X','O']
+const waysToWinArray = ['036','147','258','012','345','678','048','246']
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -44,6 +45,7 @@ class Player {
   #chars
   #playerNum
   #nextPlayer
+  
 
   constructor(playerNum) {
     this.#playerNum = playerNum
@@ -96,11 +98,13 @@ class HumanPlayer extends Player {
 class AIPlayer extends Player {
  // #playerNum
  // #nextPlayer
- #depth
+  #depth 
 
   constructor(playerNum,depth) {
     super(playerNum)
     this.#depth = depth
+    
+    
     //this.#isHumanPlayer = false
   }
 
@@ -109,8 +113,156 @@ class AIPlayer extends Player {
   }
 
   makeDecision(board) {
-   // console.log(board)
-   // controller.moveSelected(board.findIndex(el=>!el))
+
+
+    function getOpponentNum(myNum) {
+      return Math.max(((myNum)+1)%3,1)
+    }
+
+    function minimax  (board, currDepth, maxDepth,player) {
+
+
+/*
+      const checkForWinner(board) {
+    
+        for (let i = 0; i < 3; i++) {
+          if (board[i*3] && board[i*3] === board[i*3+1] && board[i*3] === board[i*3+2]) return board[i*3]
+          if (board[i] && board[i] === board[i+3] && board[i] === board[i+6]) return board[i]
+        }
+  
+        if (board[0] && board[0] === board[4] && board[0] === board[8]) return board[0]
+        if (board[2] && board[2] === board[4] && board[2] === board[6]) return board[2]
+  
+        if (board.every(el=>el)) return 3 //tie
+  
+      return -1 //game is still in play
+    }
+    */
+
+      let gameOver = controller.checkIfGameIsOver(board)
+
+      if (gameOver===1) return Infinity
+      if (gameOver===2) return -1*Infinity
+      if (gameOver===3) return 0
+
+
+
+      const makeChildren = (board,player) => {
+        //console.log('I am about to make children:')
+        //console.log('This is the current board:', board)
+        let children = []
+    
+        for (let i = 0; i < BOARD_SIZE; i++) {
+          if (!board[i]) {
+            //let child = {}
+            
+            let child = [...board]
+            //child.board = [...board]
+            child[i] = player
+            //child.move = i
+            children.push(child)
+            //console.log('Parent Board: ', board)
+            //console.log('player',player)
+            //console.log('Child Board: ', child)
+          }
+        }
+        return children
+      }
+
+
+      const numWaysPlayerWins = (num,board) => {
+        let opponent = Math.max(((num + 1) % 3),1)
+        let possibleWaystoWin = waysToWinArray.filter(w => {return w.split('').map(num=> { return board[parseInt(num)] === opponent ? 1 : 0  }).reduce((a,c)=>a+c) === 0})
+       
+
+       //let waysToWinArrayCopy = [...waysToWinArray]
+       //console.log(possibleWaystoWin)
+
+       //possibleWaystoWin = []
+       //waysToWinArray.filter(w => {
+
+      // })
+
+
+        console.log('In Num Ways to Win:',board,num,possibleWaystoWin,possibleWaystoWin.length)
+        return possibleWaystoWin.length
+    }
+  
+    const evaluateBoard = board => {
+      return numWaysPlayerWins(1,board) - numWaysPlayerWins(2,board)
+    }
+
+      if (currDepth === maxDepth) return evaluateBoard(board)
+
+      
+      let opponentNum = getOpponentNum(player)
+      let children = makeChildren(board,player)
+
+      if (children.length === 0) return evaluateBoard(board)
+
+      let maxValue = -1000000
+      let minValue = 10000000
+      for (let child of children) {
+        //console.log('making children: ', board,child)
+        let currVal = minimax (child, currDepth+1, maxDepth,opponentNum) 
+        if (currVal > maxValue) maxValue = currVal
+        if (currVal < minValue) minValue = currVal
+      } 
+
+      if (player === 1) return maxValue
+      else return minValue
+    }
+
+
+    let myNum = super.getPlayerNum()
+  // let opponentNum = Math.max(((myNum)+1)%3,1)
+  let opponentNum = getOpponentNum(myNum)
+    //console.log('my num is: ',myNum)
+    //console.log('my opponents num is: ', oppoenentNum)
+
+    let currDepth = 0
+    let maxDepth = this.#depth
+    
+    let minValue = 1000000000
+    let maxValue = -100000000
+    let iMax = -1
+    let iMin = -1
+    for (let i = 0; i < board.length; i++) {
+      if (!board[i]) {
+        let childBoard = [...board]
+        childBoard[i] = myNum
+        //console.log(board,child)
+        //console.log(childBoard, currDepth, maxDepth,opponentNum)
+        let currMinimax = minimax (childBoard, currDepth, maxDepth,opponentNum) 
+        //console.log('CurrMinimax: ',currMinimax)
+        if (currMinimax > maxValue) {maxValue = currMinimax;iMax=i}
+        if (currMinimax < minValue) {minValue = currMinimax;iMin=i}
+
+      }
+    }
+
+    if (iMax === -1) {
+      for (let i = 0; i < BOARD_SIZE; i++) {
+        if (!board[i]) {
+          iMax = i
+          break
+        }
+      }
+    }
+
+
+    if (iMin === -1) {
+      for (let i = 0; i < BOARD_SIZE; i++) {
+        if (!board[i]) {
+          iMin = i
+          break
+        }
+      }
+    }
+
+    console.log('imax/imin: ',iMin,iMax)
+    if (super.getPlayerNum()===1) view.boardClicked(iMax)
+    else view.boardClicked(iMin)
   }
 
 
@@ -290,6 +442,7 @@ player1.setNextPlayer(player2)
 player2.setNextPlayer(player1)
 
 activePlayer = player1
+activePlayer.makeDecision(Array(BOARD_SIZE))
 
 //console.log(player1)
 //console.log(player2)
@@ -331,3 +484,81 @@ function boardClicked (e) {
   view.boardClicked(boxClickedIndex)
   }
 }
+
+
+/*
+function minimax(board,currDepth,maxDepth,player) {
+    //base case -> return eval(board)
+
+
+    //makeChildren
+    //for each child, determine both min and max
+
+    //if player 1, return [max,myMove]
+    //else return [min,myMove]
+}
+*/
+/*
+
+function minimax  (board, currDepth, maxDepth,player)  {
+
+  const makeChildren = (board,player) => {
+    //console.log('I am about to make children:')
+    //console.log('This is the current board:', board)
+    let children = []
+
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      if (!board[i]) {
+        let child = {}
+        child.board = [...board]
+        child.board[i] = player
+        child.move = i
+        children.push(child)
+        console.log('Parent Board: ', board)
+        console.log('player',player)
+        console.log('Child Board: ', child.board)
+      }
+    }
+    return children
+  }
+
+  const numWaysPlayerWins = (num,board) => {
+      let opponent = Math.max(((num + 1) % 3),1)
+      let possibleWaystoWin = waysToWinArray.filter(w => {w.split('').map(num=> { return board[parseInt(num)] === opponent ? 1 : 0  }).reduce((a,c)=>a+c) === 0})
+      console.log('In Num Ways to Win:',board,num,possibleWaystoWin)
+      return possibleWaystoWin.length
+  }
+
+  const evaluateBoard = board => {
+    return numWaysPlayerWins(1,board) - numWaysPlayerWins(2,board)
+  }
+
+
+  if (currDepth === maxDepth) {let boardEval = evaluateBoard(board);
+    console.log("At max depth:",board,boardEval);
+    return [boardEval,-1]
+  }
+
+  let children = makeChildren(board,player)
+  let maxVal = -1000000000
+  let maxValI = -1
+  let minVal = 100000000
+  let minValI = -1
+  let nextPlayer = Math.max(((player + 1) % 3),1)
+
+  //console.log('Current Player is: ', player)
+  //console.log('Next Player Calculated as: ', nextPlayer)
+  for (let child of children) {
+    let currBoth = minimax(child.board,child.move,currDepth+1,maxDepth,nextPlayer)
+    let curr = currBoth[0]
+    console.log(curr)
+    if (curr > maxVal) {maxVal = curr;maxValI = child.move}
+    if (curr < minVal) {minVal = curr;minValI = child.move}
+  }
+
+  
+  if (player === 1) return [maxVal,maxValI]
+  return [minVal,minValI]
+}
+
+*/
